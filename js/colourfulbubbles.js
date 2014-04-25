efed73c238c89e67a0b236827d572946fb780275
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2014-04-25 20:27:51 +0100

    Added vanishing circles once they get small enough

--- js/colourfulbubbles.js
+++ js/colourfulbubbles.js
@@ -1,4 +1,4 @@
-var colours = ['orangered', 'gold', 'dodgerblue', 'indigo', 'red', 'lawngreen'];
+var colours = ['orangered', 'gold', 'dodgerblue', 'indigo', 'red', 'lime'];
 
 /* Add ability to peek at random index to arrays. */
 Array.prototype.random = function() {
@@ -6,27 +6,46 @@ Array.prototype.random = function() {
     return this[index];
 };
 
+var bubbles = function($tile) {
+
+    if($tile.parents().length > 14) {
+        $tile.removeClass('tile');
+        $tile.addClass('old-tile');
+        $tile.css('background-color', 'white');
+        return;
+    }
+
+    var newTiles = '<div class="container"><div class="row"><div class="tile"></div><div class="tile"></div></div><div class="row"><div class="tile"></div><div class="tile"></div></div></div>';
+
+//    $tile.append(newTiles).fadeIn(300);
+    var $newTiles = $(newTiles);
+
+    $newTiles.find('.tile').each(function() {
+        $(this).css('background-color', colours.random())
+    });
+
+    $newTiles.appendTo($tile);
+
+    $tile.removeClass('tile');
+    $tile.addClass('old-tile');
+
+
+};
+
 $(document).ready(function() {
 
-    $('#tiles-board').on('mouseenter', '.tile', function() {
+    var $tilesBoard = $('#tiles-board');
 
-        var $this = $(this);
-
-        if($this.parents().length > 14) {
-            $this.removeClass('tile');
-            $this.addClass('old-tile');
-            return;
-        }
-
-        $this.empty();
-        $this.append('<div class="container"><div class="row"><div class="tile"></div><div class="tile"></div></div><div class="row"><div class="tile"></div><div class="tile"></div></div></div>');
-
-        $this.removeClass('tile');
-        $this.addClass('old-tile');
-
-        $this.find('.tile').each(function() {
-            $(this).css('background-color', colours.random())
-        });
-
+    /* Give initial colours. */
+    $('.tile').each(function() {
+        $(this).css('background-color', colours.random());
     });
+
+    $tilesBoard.on('mouseenter', '.tile', function() {
+        bubbles($(this));
+    });
+
+    $tilesBoard.on('click', '.tile', function() {
+        bubbles($(this));
+    })
 });
